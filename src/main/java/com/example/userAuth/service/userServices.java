ec26b6dbfad0f138526bcf9169d2cc860b040b9f
Pratik-sys <pithore.pratik@gmail.com>
2024-03-13 12:34:41 +0530

    hash the password with bcrypt

--- src/main/java/com/example/userAuth/service/userServices.java
+++ src/main/java/com/example/userAuth/service/userServices.java
@@ -2,14 +2,22 @@ package com.example.userAuth.service;
 
 import com.example.userAuth.model.users;
 import com.example.userAuth.repository.userRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class userServices {
     @Autowired
     private userRepository userRepo;
-    public users addUsers(users user){return userRepo.save(user);}
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
+    public users addUsers(users user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepo.save(user);}
 
 }
